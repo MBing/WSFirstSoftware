4a975ac843d82e22724dbc6a71207516c603dd7b
Martin Bing <bing.martin.photography@gmail.com>
2015-11-16 03:21:42 +0100

    Fix some changes to contact page

--- contents/js/script.js
+++ contents/js/script.js
@@ -4,14 +4,17 @@
     /********/
     /* MENU */
     /********/
+    $('.page').hide();
+    var initialPage = document.location.hash || '#home';
+    $(initialPage).show();
 
     /* Main Menu Links*/
-    $('.menu-title > a').on('click', function () {
-        $('.page')
-            .removeClass('show')
-            .hide();
-        $($(this).attr('href')).show();
-        $($(this).attr('href')).addClass('show');
+    $(".menu-title > a").on('click', function () {
+        console.log(!$(this).parent().hasClass('submenu'));
+        if (!$(this).parent().hasClass('submenu')) {
+            $('.page').hide();
+            $($(this).attr('href')).show();
+        }
     });
     /* Submenu Links */
     $('.submenu-item > a').on('click', function () {
@@ -66,4 +69,59 @@
     /* SLIDER */
     $(".slider img:first-child").show();
 
+
+    /* CONTACT PAGE */
+    var toInputField = function (key) {
+        return '<input name="' + key + '">';
+    };
+    var toTextArea = function (key) {
+        return '<textarea name="' + key + '"></textarea>';
+    };
+    var toButton = function (key) {
+        return '<input type="button" value="' + key + '">';
+    };
+    var toSelect = function (key, list) {
+        console.log();
+        var sel = ["<select name='"+ $(list).text() +"'>"];
+        list.each(function (index, item) {
+            var itemTitle = $(item).text();
+            //var optie = '<option value="' + itemTitle + '">' + itemTitle + '</option>';
+            var optie = "<option value='" + itemTitle + "'>";
+            sel.push(optie);
+            console.log("optie:", optie);
+        })
+        sel.push('</ul>');
+        list.replaceWith(sel.join(""));
+        //return '<input type="button" value="' + key + '">';
+    };
+
+    $('#contact h3').each(function (key, val) {
+        var getFirstItem = $(this).siblings('p').first();
+        var output, objTitle = $(this).text();
+        switch(getFirstItem.text()) {
+            case 'input':
+                output = toInputField(objTitle);
+                break;
+            case 'textarea':
+                output = toTextArea(objTitle);
+                break;
+            case 'button':
+                output = toButton(objTitle);
+                break;
+            default:
+                // create select
+                var newSelect = $(this).siblings('ul').first();
+                if (newSelect) toSelect(objTitle, newSelect)
+                output = "";
+                break;
+        }
+        getFirstItem.replaceWith(output);
+        //if (getFirstItem.text() === 'input') {
+        //    getFirstItem.replaceWith(toInputField($(this).text()));
+        //} else if (getFirstItem.text() === 'textarea') {
+        //    getFirstItem.replaceWith(toTextArea($(this).text()));
+        //} else if (getFirstItem.text() === 'button') {
+        //    getFirstItem.replaceWith(toButton($(this).text()));
+        //}
+    });
 })();
--- contents/pages/contact/index.md
+++ contents/pages/contact/index.md
@@ -62,3 +62,5 @@ input
 ### Boekhoudpakket
 input
 
+### VERZENDEN
+button
